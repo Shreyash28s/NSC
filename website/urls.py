dad5e1ff8c297f593a452c8c84e07a0ef3436fdb
Shreyash28s <shreyash.sowaruth@outlook.com>
2024-04-09 11:04:51 +0400

    navbar added

--- website/urls.py
+++ website/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("about", views.about),
-    path("boardpage", views.boardpage),
-    path("events", views.events),
-    path("homepage", views.homepage),
-    path("membership", views.membership),
+    path("about", views.about, name="about"),
+    path("boardpage", views.boardpage, name ="boardpage"),
+    path("events", views.events, name="events"),
+    path("homepage", views.homepage, name="home"),
+    path("membership", views.membership, name="membership"),
     path("register", views.register),
-    path("sponsorship", views.sponsorship)
+    path("sponsorship", views.sponsorship, name="sponsorship")
 ]
